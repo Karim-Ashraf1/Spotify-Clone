a66ba00afadb57c6f3ecc5f58db5a5d93cc27ce5
zeyad waleed <s-zeyad.nabil@zewailcity.edu.eg>
2025-04-10 09:07:52 +0200

    solving frontend problem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,12 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
-  
-  return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-  )
+  return <div>nadi karhy bakrah Karim Ashraf</div>;
 }
 
-export default App
+export default App;

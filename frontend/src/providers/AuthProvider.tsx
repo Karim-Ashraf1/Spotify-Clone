eb4b7349c766262ff6379e86e784b157977d4fe4
kimosoft1 <kariiimashraf.1@gmail.com>
2025-05-12 16:19:14 +0300

    test: vercel build issues

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -37,7 +37,7 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   if (loading)
     return (
       <div className="h-screen w-full flex items-center justify-center">
-        <Loader className="size-8 text-emerald-500 animate-spin" />
+        <Loader className="w-8 h-8 text-emerald-500 animate-spin" />
       </div>
     );
 

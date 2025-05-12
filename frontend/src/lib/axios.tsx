21b04a1eefe4565f5487e5e442ba7256b48585ba
kimosoft1 <kariiimashraf.1@gmail.com>
2025-05-12 19:53:13 +0300

    Fix axios baseURL for production

--- frontend/src/lib/axios.tsx
+++ frontend/src/lib/axios.tsx
@@ -4,5 +4,5 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "/api",
+      : "https://spotify-clone-backend-pied.vercel.app/api",
 });
